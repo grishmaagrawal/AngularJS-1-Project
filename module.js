f9ee93e70175ff065226abbd30e6d9e3fa9dc12e
grishmaagrawal <44696830+grishmaagrawal@users.noreply.github.com>
2018-11-02 14:55:23 +0530

    Add angularJS1

---
+++ module.js
@@ -0,0 +1,7 @@
+var app = angular.module('myApp',[]);
+app.controller('stateList', function($scope){
+    $scope.state = ['Andhra Pradesh','Arunachal Pradesh','Assam','Bihar','Chhattisgarh',
+    'Goa','Gujarat','Haryana','Himachal Pradesh','Jammu & Kashmir','Jharkhand','Karnataka',
+    'Kerala','Madhya Pradesh','Maharashtra','Manipur','Meghalaya'];
+    
+});
